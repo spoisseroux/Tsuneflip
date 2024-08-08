db7f92ab02248bea4e80c384978f7bb2f0621aa5
John Corio <44953944+corioj@users.noreply.github.com>
2024-08-08 02:14:03 -0400

    3rd person movement
    Finished camera relative movement. Next is fiddling with camera collisions and NPCs?

--- Assets/Scripts/EntitySystem/PlayerMovement.cs
+++ Assets/Scripts/EntitySystem/PlayerMovement.cs
@@ -13,13 +13,11 @@ public class PlayerMovement : EntityMovement
 
     [SerializeField] public Vector3 currentDirection; // for storing last frame's info
     [SerializeField] public Vector3 workingDirection; // edits new frame's info
-    public Vector3 relativeRightVector;
-    public Vector3 relativeForwardVector;
+    public Vector3 movementRightVector; // x plane movement
+    public Vector3 movementForwardVector; // z plane movement
     public Vector3 movementUpVector; // how do we actually keep this in world space???
 
     [SerializeField] private BoxCollider footCollider; // for grounded check & enemy head collider check
-
-    private float turnSmoothVelocity;
 
     // raycast below for things
 
@@ -42,16 +40,16 @@ public class PlayerMovement : EntityMovement
         ApplyGravity();
         // apply jump to workingdir
         Jump();
-        // set rotation
-        SetRotation();
         // apply movement to workingdir
         Move(playerInput.currentInput.planeMove);
         // combine all into workingDirection
         SetWorkingDirectionVector();
+        // set rotation based on currentDir
+        HandleRotation();
         // set currentdir
         currentDirection = workingDirection; // update our current direction to the calculated magnitude
         // move
-        character.Move(currentDirection * Time.deltaTime);
+        character.Move(currentDirection * Time.deltaTime); // EXPECTING WORLD SPACE VECTOR
     }
     #endregion
 
@@ -60,8 +58,6 @@ public class PlayerMovement : EntityMovement
     {
         if (playerInput.currentInput.jumpMove && grounded)
         {
-            //workingDirection.Set(workingDirection.x, constants.jumpSpeed, workingDirection.z);
-
             movementUpVector = Vector3.up * constants.jumpSpeed;
         }
     }
@@ -70,44 +66,57 @@ public class PlayerMovement : EntityMovement
     public override void Move(Vector2 move)
     {
         // create movement basis vectors from camera's coordinate space
-        Vector3 right = transform.InverseTransformVector(thirdPersonCamera.transform.right);
-        Vector3 forward = transform.InverseTransformVector(thirdPersonCamera.transform.forward);
-        right.y = 0;
-        forward.y = 0;
-        right = right.normalized;
-        forward = forward.normalized;
-
-        relativeRightVector = move.x * constants.moveSpeed * right;
-        relativeForwardVector = move.y * constants.moveSpeed * forward;
-        
-
-        //SetWorkingX(move.x * constants.moveSpeed);
-        //SetWorkingZ(move.y * constants.moveSpeed);
+        Vector3 cameraRight = thirdPersonCamera.transform.right;
+        Vector3 cameraForward = thirdPersonCamera.transform.forward;
+        // eliminate y components & re-normalize
+        cameraRight.y = 0;
+        cameraForward.y = 0;
+        cameraRight = cameraRight.normalized;
+        cameraForward = cameraForward.normalized;
+        // translate our movement inputs into camera space
+        movementRightVector = move.x * constants.moveSpeed * cameraRight;
+        movementForwardVector = move.y * constants.moveSpeed * cameraForward;
     }
+
+    /*
+    private Vector3 ConvertToCameraSpace(Vector3 vectorToRotate)
+    {
+        Vector3 cameraRight = thirdPersonCamera.transform.right;//Camera.main.transform.right;
+        Vector3 cameraForward = thirdPersonCamera.transform.forward;
+
+        cameraRight.y = 0;
+        cameraForward.y = 0;
+
+        cameraRight = cameraRight.normalized;
+        cameraForward = cameraForward.normalized;
+
+        // rotate to camera space
+        Vector3 cameraForwardX = vectorToRotate.x * cameraRight;
+        Vector3 cameraForwardZ = vectorToRotate.z * cameraForward;
+
+        return cameraForwardX + cameraForwardZ;
+    }
+    */
 
     protected override void ApplyGravity()
     {
         if (!grounded)
         {
-            // apply ascent or descent gravity
             float newVel = workingDirection.y;
-            // ascent
+            // apply ascent or descent gravity
             if (newVel >= 0)
             {
                 newVel += (constants.ascentGravity * Time.deltaTime);
             }
-            // descent
             else
             {
                 newVel += (constants.descentGravity * Time.deltaTime);
             }
             newVel = Mathf.Max(newVel, -20.0f);
-            //workingDirection.Set(workingDirection.x, newVel, workingDirection.z);
             movementUpVector = Vector3.up * newVel;
         }
         else
         {
-            //workingDirection.Set(workingDirection.x, constants.groundedGravity, workingDirection.z);
             movementUpVector = Vector3.up * constants.groundedGravity;
         }
     }
@@ -119,16 +128,22 @@ public class PlayerMovement : EntityMovement
     }
 
 
-    public void SetRotation()
+    public void HandleRotation()
     {
         // set a look direction
         if (playerInput.currentInput.planeMove.magnitude >= 0.05f)
         {
-            Debug.Log("Update facing angle");
-            // smooth damp to target angle
-            float targetAngle = Mathf.Atan2(currentDirection.x, currentDirection.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, constants.turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0, angle, 0);
+            // make desired position to look at
+            Vector3 lookAtDir;
+            lookAtDir.x = workingDirection.x;
+            lookAtDir.y = 0f;
+            lookAtDir.z = workingDirection.z;
+            // current rotation
+            Quaternion facingDir = transform.rotation;
+            // target rotation
+            Quaternion target = Quaternion.LookRotation(lookAtDir);
+            // slerp
+            transform.rotation = Quaternion.Slerp(facingDir, target, constants.turnSpeed * Time.deltaTime);
         }
         // set facing direction
         facingDir = transform.rotation;
@@ -137,25 +152,6 @@ public class PlayerMovement : EntityMovement
 
     private void SetWorkingDirectionVector()
     {
-        workingDirection = relativeRightVector + relativeForwardVector + movementUpVector;
+        workingDirection = movementRightVector + movementForwardVector + movementUpVector;
     }
-
-
-
-
-
-
-
-    private void SetWorkingX(float x)
-    {
-        // right + up and left vectors
-        Vector3 xDir = thirdPersonCamera.transform.right * x;
-        workingDirection.Set(x, workingDirection.y, workingDirection.z);
-    }
-
-    private void SetWorkingZ(float z)
-    {
-        workingDirection.Set(workingDirection.x, workingDirection.y, z);
-    }
-
 }
